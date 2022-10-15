5e839995d2a0359f8711acde585142934499b390
wavwav <cu_0918@yahoo.co.jp>
2022-10-16 02:05:35 +0900

    topbar作成

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TopBar from './conponents/TopBar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <TopBar />
     <App />
   </React.StrictMode>
 );
